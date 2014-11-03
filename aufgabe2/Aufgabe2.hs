c18894197c39b8f2b2e793eb3e8157e4a1aab98b
Raphael Gruber <raphi011@gmail.com>
2014-11-03 19:53:23 +0100

    rewrote maxWordLength

--- aufgabe2/Aufgabe2.hs
+++ aufgabe2/Aufgabe2.hs
@@ -1,19 +1,26 @@
 import Data.List
-import Data.Function
+import Data.Ord
 
 type Line = String
 type Text = [Line]
 
-words' :: Text -> Line
+words' :: Text -> [String]
 words' t = words $ concat t
 
 maxWordLength :: Text -> Integer
-maxWordLength t = fromIntegral(length $ maximumBy (compare `on` length) $  words' t)
+maxWordLength t = fromIntegral(length $ maximumBy (comparing length) $  words' t)
+-- maxWordLength t = fromIntegral(length $ maximumBy (\x y -> (length x < length y) $  words' t)
+--maxWordLength t = fromIntegral(length $ maximumBy (compare `on` length) $  words' t)
+
+countUniques :: (Eq a) => [a] -> Int
+countUniques = length . nub
+
+hasLength :: Integer -> String -> Bool
+hasLength l s = length s == fromIntegral(l)
 
 numWordOcc :: Text -> Integer -> Integer
 numWordOcc t n | n < 1 = -1
-numWordOcc t n =  length . nub $ filter (\x (length x == n)) $ words' t
-
+numWordOcc t n =  fromIntegral(countUniques $ filter (hasLength n) $ words' t)
 
 reverseCheck :: Text -> Bool
 reverseCheck t = False
