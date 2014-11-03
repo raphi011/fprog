129d0d7c306cb1940752b5cbbadaa106a6a4871b
Raphael Gruber <raphi011@gmail.com>
2014-11-03 19:14:39 +0100

    started with numWordOcc

--- aufgabe2/Aufgabe2.hs
+++ aufgabe2/Aufgabe2.hs
@@ -4,14 +4,15 @@ import Data.Function
 type Line = String
 type Text = [Line]
 
+words' :: Text -> Line
+words' t = words $ concat t
+
 maxWordLength :: Text -> Integer
-maxWordLength t = fromIntegral(length $ maximumBy (compare `on` length) $ words $ concat t)
---maxWordLength t = words $ concat t
-
-
+maxWordLength t = fromIntegral(length $ maximumBy (compare `on` length) $  words' t)
 
 numWordOcc :: Text -> Integer -> Integer
 numWordOcc t n | n < 1 = -1
+numWordOcc t n =  length . nub $ filter (\x (length x == n)) $ words' t
 
 
 reverseCheck :: Text -> Bool
